f157a56212eb8079e8bf35a9fd999d5ed3560812
小室拳作 <192174995+fistkk71@users.noreply.github.com>
2025-09-06 18:49:28 +0900

    音量調整

--- game1/tetris.js
+++ game1/tetris.js
@@ -16,7 +16,7 @@ const SFX = (() => {
   const LS_KEY = 'tetris_audio_v1';
 
   // 初期値（好みで変更可）
-  const state = { master: 0.30, bgm: 0.20, sfx: 0.55 };
+  const state = { master: 0.25, bgm: 0.18, sfx: 0.45 };
 
   try {
     const saved = JSON.parse(localStorage.getItem(LS_KEY));
